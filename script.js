58ce93f1f0743d04b6fed892ad620277f9a9735b
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-28 16:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstChar(text) {
   // your code here
-	if(text===' ') return " ";
+	if(text===' ') return '' ;
 	return text.trim().text[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
 
